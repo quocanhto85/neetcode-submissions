2a2b89fc07bc26e787a3f7f48efb764da7919e9c
To Phuong Quoc Anh <36063411+quocanhto85@users.noreply.github.com>
2026-06-09 21:37:11 +0930

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            groups[key].append(word)
+
+        return list(groups.values())
+
+
+strs = ["act","pots","tops","cat","stop","hat"]
+sol = Solution()
+sol.groupAnagrams(strs)
